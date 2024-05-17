1ac7ae33212d6b40908f661c21d870a2e895e1e5
Amir JCS <amirmahdyjdx@gmail.com>
2024-05-17 16:10:25 +0330

    refactor(core): make function and constants private

--- src/commprs_core.py
+++ src/commprs_core.py
@@ -1,10 +1,10 @@
-import re
+import re as __re
 
-_COMMAND_NAME_EXTRAXTION_RGEX = r"(\(\"?).+(\"?\))"
-_COMMAND_ARGS_SECTION_EXTRACTOR_REGEX = r"\(\"?.+\"?\)"  # the pattern \(\"?.+\"?\) gives us anything between two (), like sum(12) => the result will be 13
+__COMMAND_NAME_EXTRAXTION_RGEX = r"(\(\"?).+(\"?\))"
+__COMMAND_ARGS_SECTION_EXTRACTOR_REGEX = r"\(\"?.+\"?\)"  # the pattern \(\"?.+\"?\) gives us anything between two (), like sum(12) => the result will be 13
 
 # [Just for Test] a dictionary of commands, it will be changed into anouther format
-_comDict = {
+__comDict = {
     "sub": lambda args: int(args[0]) - int(args[1]),
     "sum": lambda args: int(args[0]) + int(args[1]),
     "mul": lambda args: int(args[0]) * int(args[1]),
@@ -19,13 +19,13 @@ _comDict = {
 }
 
 
-def extractCommandName(mnCom):
-    return re.sub(_COMMAND_NAME_EXTRAXTION_RGEX, "", mnCom)  # to get the command name
+def __extractCommandName(mnCom):
+    return __re.sub(__COMMAND_NAME_EXTRAXTION_RGEX, "", mnCom)  # to get the command name
 
 
-def extractCommandArguments(mnCom):
+def __extractCommandArguments(mnCom):
     outs = []
-    args = re.findall(_COMMAND_ARGS_SECTION_EXTRACTOR_REGEX, (mnCom))
+    args = __re.findall(__COMMAND_ARGS_SECTION_EXTRACTOR_REGEX, (mnCom))
     if len(args) == 0:
         return []
     for a in args[0].split(","):
@@ -33,28 +33,24 @@ def extractCommandArguments(mnCom):
     return outs
     # to get the arguments section
 
+def __splitComm(com):
+    return __re.split(r"\;+\b", com)  # get the set of commands that inputed by user
 
 def runCommand(_commName, _args):
-    return _comDict[_commName](_args)
-
-
-def splitComm(com):
-    return re.split(r"\;+\b", com)  # get the set of commands that inputed by user
-
+    return __comDict[_commName](_args)
 
 def runAll(com):
-    _comSt = splitComm(com)
+    _comSt = __splitComm(com)
     for _mnCom in _comSt:
-        args = extractCommandArguments(_mnCom.replace(" ", ""))
-        commName = extractCommandName(
+        args = __extractCommandArguments(_mnCom.replace(" ", ""))
+        commName = __extractCommandName(
             _mnCom
         )  # Call the command name extractor function
         yield runCommand(commName, args)
 
-
 def runFirst(com):
-    _comSt = splitComm(com)
+    _comSt = __splitComm(com)
     _mnCom = _comSt[0] # only first run the command 
-    args = extractCommandArguments(_mnCom.replace(" ", ""))
-    commName = extractCommandName(_mnCom)  # Call the command name extractor function
+    args = __extractCommandArguments(_mnCom.replace(" ", ""))
+    commName = __extractCommandName(_mnCom)  # Call the command name extractor function
     return runCommand(commName, args)
