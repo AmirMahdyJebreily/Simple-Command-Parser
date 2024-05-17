bf6ba0e93c5abd28aad214b9997d3245ca0fd95d
Amir JCS <amirmahdyjdx@gmail.com>
2024-05-17 15:52:37 +0330

    test(core): runFirst function test for simple commands

--- src/test.py
+++ src/test.py
@@ -6,8 +6,8 @@ class commTest(unittest.TestCase):
     def test_runCommand(self):
         self.assertEqual(comm.runCommand("sum", ["12", "13"]), 25)
 
-    def test_runAll(self):
-        self.assertEqual(list(comm.runAll("sum(12, 13)")), [25])
+    def test_runFirst(self):
+        self.assertEqual(comm.runFirst("sum(12, 13)"), 25)
 
     def test_runAll_MultiCommands(self):
         self.assertEqual(list(comm.runAll("sum(12, 13);mul(1, 2)")), [25, 2])
