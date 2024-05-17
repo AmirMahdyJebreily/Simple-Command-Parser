fb2dffaa1409b49c652f9106c79c29d58a4e272b
Amir JCS <amirmahdyjdx@gmail.com>
2024-05-17 17:35:35 +0330

    refactor: change main file styles

--- src/main.py
+++ src/main.py
@@ -1,7 +1,9 @@
 from commprs_core import runAll
 
+print("Hi, Wellcome to CodeAgha Simple Command Parser")
+print("Github repo :", "https://github.com/AmirMahdyJebreily/Simple-Command-Parser\n")
 while True:
     com = input("> ")
     res = runAll(com)
     for r in res:
-        print(r)
+        print(":", r)
