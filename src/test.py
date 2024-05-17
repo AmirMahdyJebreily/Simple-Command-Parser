8e4eaca4686081c8e2cf9e74a038a322ecfc65be
Amir JCS <amirmahdyjdx@gmail.com>
2024-05-17 14:15:48 +0330

    test(core): runAll function test

--- src/test.py
+++ src/test.py
@@ -5,6 +5,10 @@ import commprs_core as comm
 class commTest (unittest.TestCase):
     def test_runCommand(self):
         self.assertEqual(comm.runCommand("sum", ['12', '13']), 25)
+    
+    def test_runAll(self):
+        self.assertEqual(comm.runAll("sum(12, 13)"), 25)
+        
 
 if __name__ == '__main__':
     unittest.main()
