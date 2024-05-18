384a51362f34f9dffc56331e683a7871ef767dd3
Amir JCS <amirmahdyjdx@gmail.com>
2024-05-18 22:22:16 +0330

    chore(main): minor comands and cleaning code

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+# CodeAgha 2023
 from src.commprs_core import runAll
 
 print("Hi, Wellcome to CodeAgha Simple Command Parser")
-print("Github repo :", "https://github.com/AmirMahdyJebreily/Simple-Command-Parser\n")
+print("Github repo:", "https://github.com/AmirMahdyJebreily/Simple-Command-Parser\n")
 while True:
-    com = input("> ")
-    res = runAll(com)
-    for r in res:
-        if r != None:
-            print(":", r)
+    commands = input("> ") # get commands from imports
+    results = runAll(commands) # run all commands with "src.commprs_core" function runAll
+    for res in results: # iterate yield results
+        if res != None: # for filter printing noRes Functions
+            print(":", res) # print results ;)
