6ecb4ca2ca1d9f8d10ad22a7778775636c355108
Amir JCS <amirmahdyjdx@gmail.com>
2024-05-19 14:32:09 +0330

    feat(core): add variable definding functions

--- src/commprs_core.py
+++ src/commprs_core.py
@@ -6,12 +6,20 @@ __VAR_DETECTION = r"\$\w+\s*\<{1}\=\s*.+"
 __COMMAND_NAME_EXTRAXTION_RGEX = r"(\(\"?).+(\"?\))"
 __COMMAND_ARGS_SECTION_EXTRACTOR_REGEX = r"\(\"?.+\"?\)"  # the pattern \(\"?.+\"?\) gives us anything between two (), like sum(12) => the result will be 13
 
+# dictionary of variables
+__varsDict = {
+    "varsion": "0.0.1", # default variable
+}
+
 # [Just for Test] a dictionary of commands, it will be changed into anouther format
 __comDict = {
     "sub": lambda args: int(args[0]) - int(args[1]),
     "sum": lambda args: int(args[0]) + int(args[1]),
     "mul": lambda args: int(args[0]) * int(args[1]),
     "dvd": lambda args: int(args[0]) / int(args[1]),
+
+    # syntax commands : 
+    "var": lambda args: __defVariable(args[0], args[1]),
 
     # default commands : 
     "hi": lambda args: "hi there, this is codeagha's simple command parser based on regex !"
@@ -23,11 +31,18 @@ __comDict = {
     "exit": lambda args: exit(),
     "clear": lambda args: tool.runNoRes(os.system,'cls' if os.name == 'nt' else 'clear'),
 
-    # default for fun functions : 
+    # default for fun commands : 
     "!mkemptscrn": lambda args: chr(27) + "[2J" + ' '.join(args),
     "!pishi": lambda args: "meow ;) " + " ".join(args), # for my friend, pishi :)
 }
 
+def __defVariable(varname: str, value): # defind variable
+    if(__re.fullmatch(r"[a-z]+\w*", varname)): 
+        __varsDict[varname] = value
+        print(": [OK]:", varname, "=", __varsDict[varname])
+        # vriable added to __varsDict
+    else:
+        print(": [SyntaxError]: Var name is unvalid", varname) # error handeling
 
 def __extractCommandName(mnCom):
     return __re.sub(__COMMAND_NAME_EXTRAXTION_RGEX, "", mnCom)  # to get the command name
